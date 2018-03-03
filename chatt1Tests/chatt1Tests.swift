118003c00aacaaf7d37303f58cc6096d7ed35f51
sakkarin panpanich <sakkarinpanpanich@sakkarins-MacBook-Air.local>
2018-03-04 00:47:47 +0700

    Initial Commit

---
+++ chatt1Tests/chatt1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  chatt1Tests.swift
+//  chatt1Tests
+//
+//  Created by sakkarin panpanich on 3/4/2561 BE.
+//  Copyright © 2561 sakkarin panpanich. All rights reserved.
+//
+
+import XCTest
+@testable import chatt1
+
+class chatt1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
